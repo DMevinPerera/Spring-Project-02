9b8f8d82e04466321ebca13207124d199f96115e
MevinPerera <87105802+DMevinPerera@users.noreply.github.com>
2024-11-15 23:46:27 +0530

    Constructors, Setters, Field Injection

--- src/main/java/com/SpringProject1/Spring_Project/examples/a1/DepInjectionLauncherApplication.java
+++ src/main/java/com/SpringProject1/Spring_Project/examples/a1/DepInjectionLauncherApplication.java
@@ -11,13 +11,33 @@ import java.util.Arrays;
 @Component
 class YourBusinessClass{
 
-    @Autowired
+
     Dependency1 dependency1;
 
-    @Autowired
     Dependency2 dependency2;
 
-            public String toString(){
+    @Autowired
+    public YourBusinessClass(Dependency1 dependency1, Dependency2 dependency2) {
+        super();
+        System.out.println("Constructor Injection - YourBusinessClass");
+        this.dependency1 = dependency1;
+        this.dependency2 = dependency2;
+    }
+
+    //    @Autowired
+//    public void setDependency1(Dependency1 dependency1) {
+//        System.out.println("Setter Injection - setDependancy1");
+//        this.dependency1 = dependency1;
+//    }
+//
+//    @Autowired
+//    public void setDependency2(Dependency2 dependency2) {
+//
+//        System.out.println("Setter Injection - setDependancy2");
+//        this.dependency2 = dependency2;
+//    }
+
+    public String toString(){
                 return "Using "+ dependency1 + " and" + dependency2;
             }
 }
