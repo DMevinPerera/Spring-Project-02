3e4e090f5932f46753fb6fa7de873cf2f386b69e
MevinPerera <87105802+DMevinPerera@users.noreply.github.com>
2024-11-19 22:50:50 +0530

    Spring Bean Scopes

---
+++ src/main/java/com/SpringProject1/Spring_Project/examples/e1/a0/BeanScopesLauncherApplication.java
@@ -0,0 +1,44 @@
+package com.SpringProject1.Spring_Project.examples.e1.a0;
+
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Component
+class NormalClass {
+
+}
+
+@Scope(value = ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+@Component
+class PrototypeClass {
+
+}
+
+@Configuration
+@ComponentScan("com.SpringProject1.Spring_Project.examples.e1")
+public class BeanScopesLauncherApplication {
+
+    public static void main(String[] args) {
+
+        var context = new AnnotationConfigApplicationContext(BeanScopesLauncherApplication.class);
+        {
+            System.out.println(context.getBean(NormalClass.class));
+            System.out.println(context.getBean(NormalClass.class));
+            System.out.println(context.getBean(NormalClass.class));
+            System.out.println(context.getBean(NormalClass.class));
+
+
+            System.out.println(context.getBean(PrototypeClass.class));
+            System.out.println(context.getBean(PrototypeClass.class));
+            System.out.println(context.getBean(PrototypeClass.class));
+
+        }
+    }
+
+}
